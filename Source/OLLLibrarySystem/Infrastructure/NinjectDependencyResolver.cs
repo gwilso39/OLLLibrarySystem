cf2575b45987d7476577b90d4b33df2eb304a04b
Greg Wilson <greg.wilson5@outlook.com>
2018-04-22 18:24:29 -0400

    Created a mock repository in the NinjectDependenceResolver.

--- Source/OLLLibrarySystem/Infrastructure/NinjectDependencyResolver.cs
+++ Source/OLLLibrarySystem/Infrastructure/NinjectDependencyResolver.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Ninject;
+using Moq;
+using OLLLibrarySystem.Domain.Abstract;
+using OLLLibrarySystem.Domain.Entities;
 
 namespace OLLLibrarySystem.WebUI.Infrastructure
 {
@@ -29,6 +32,23 @@ namespace OLLLibrarySystem.WebUI.Infrastructure
 
         private void AddBindings()
         {
+            Mock<IEntitiesRepository> mock = new Mock<IEntitiesRepository>();
+            mock.Setup(m => m.Book).Returns(new List<Book>
+            {
+                new Book {  BookID = 1, LowerLexile = 1, UpperLexile = 3, Location = 1, CheckedOutIn = 1,
+                            RecAge = 3, Title = "Book One", Author = "My Author", Genre = "Fiction",
+                            Description = "A book about one", Photo = "image here", ReplacementCost = 2,
+                            ISBN = 16957852},
+                new Book {  BookID = 2, LowerLexile = 2, UpperLexile = 3, Location = 1, CheckedOutIn = 1,
+                            RecAge = 3, Title = "Book Two", Author = "My Author", Genre = "Non-Fiction",
+                            Description = "A book about two", Photo = "image here", ReplacementCost = 2,
+                            ISBN = 16957850},
+                new Book {  BookID = 3, LowerLexile = 3, UpperLexile = 3, Location = 1, CheckedOutIn = 1,
+                            RecAge = 3, Title = "Book Three", Author = "My Author", Genre = "Fiction",
+                            Description = "A book about three", Photo = "image here", ReplacementCost = 2,
+                            ISBN = 16957848}
+                });
+            kernel.Bind<IEntitiesRepository>().ToConstant(mock.Object);
             //kernel.Bind<IProductRepository>().To<EFProductRepository>();
         }
     }
